4b2f7428bc0f236a2aa18a1f491631f7c86b8c39
Kwang Su Choi <ksuchoi1219@gmail.com>
2016-10-31 14:50:48 -0400

    added import

--- app/src/main/java/com/example/kangsu/inventoryquery/Inventory.java
+++ app/src/main/java/com/example/kangsu/inventoryquery/Inventory.java
@@ -1,113 +1,25 @@
 package com.example.kangsu.inventoryquery;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
+
 
 
 import java.util.Calendar;
 
 
-public class Inventory extends AppCompatActivity implements OnClickListener {
+public class Inventory extends AppCompatActivity {
 
-//Calendar: Variables
-    private Button changeDate;
-    private int mYear;
-    private int mMonth;
-    private int mDay;
-    static final int DATE_PICKER_ID = 1111;
-    private TextView displayDate;
 
-//Barcode Scan: Variables
-    private Button scannerButton;
-    private TextView displayBarcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_dashboard);
 
-        //Calendar: Buttons
-        changeDate = (Button)findViewById(R.id.pickDate);
-        displayDate = (TextView)findViewById(R.id.userDate);
-        final Calendar c = Calendar.getInstance();
-        mYear  = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay   = c.get(Calendar.DAY_OF_MONTH);
 
-        //Barcode Scan: Buttons
-        scannerButton = (Button) findViewById(R.id.scanButton);
-        displayBarcode = (TextView) findViewById(R.id.userBarcode);
-        scannerButton.setOnClickListener(this);
-
-
-        //Calendar: Functions
-        displayDate.setText(new StringBuilder()
-                .append(mMonth + 1).append("-").append(mDay).append("-")
-                .append(mYear).append(" "));
-        changeDate.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                showDialog(DATE_PICKER_ID);
-
-            }
-
-        });
 
     }
-    //Calendar: Date Picker
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DATE_PICKER_ID:
-                return new DatePickerDialog(this, pickerListener, mYear, mMonth, mDay);
-        }
-        return null;
-    }
-    //Calendar: Date Picker (display date)
-    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener() {
 
-        // when dialog box is closed, below method will be called.
-        @Override
-        public void onDateSet(DatePicker view, int selectedYear,
-                              int selectedMonth, int selectedDay) {
-            mYear  = selectedYear;
-            mMonth = selectedMonth;
-            mDay   = selectedDay;
-            displayDate.setText(new StringBuilder().append(mMonth + 1)
-                    .append("-").append(mDay).append("-").append(mYear)
-                    .append(" "));
-
-        }
-    };
-    //Barcode Scan: Onclick Function
-    public void onClick(View v) {
-        if (v.getId() == R.id.scanButton) {
-            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-            scanIntegrator.initiateScan();
-        }
-    }
-    //Barcode Scan: Scanner
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanningResult != null) {
-            String scanContent = scanningResult.getContents();
-            displayBarcode.setText(scanContent);
-        } else {
-            Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-    }
 }
 
 
