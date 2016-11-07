1d7dd2864ecf7cc7d72d8174e0819ef86361e275
Kwang Su Choi <ksuchoi1219@gmail.com>
2016-11-07 08:59:25 -0500

    added DB

---
+++ app/src/main/java/com/example/kangsu/inventoryquery/ConnectionClass.java
@@ -0,0 +1,47 @@
+package com.example.kangsu.inventoryquery;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * Created by KangSu on 11/2/2016.
+ */
+
+public class ConnectionClass {
+
+
+    String ip = "192.168.1.124";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "Andro1";
+    String un = "apptest";
+    String password = "1234okok";
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Log.e("t", "Connecting");
+            Class.forName(classs).newInstance();
+            Log.e("t", "Driver connected");
+            Log.e("t", "DB connecting");
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";" + "databaseName=" + db + ";user=" + un + ";password=" + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+            Log.e("t", "Connected");
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
--- app/src/main/java/com/example/kangsu/inventoryquery/Login.java
+++ app/src/main/java/com/example/kangsu/inventoryquery/Login.java
@@ -1,47 +1,110 @@
 package com.example.kangsu.inventoryquery;
 
-import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class Login extends AppCompatActivity{
 
-    private Button lButton;
+
+    private ConnectionClass connectionClass;
+    private EditText edtuserid,edtpass;
+    private Button btnlogin;
+    private ProgressBar pbbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        addListenerOnButton();
 
-        lButton = (Button)findViewById(R.id.loginButton);
-        lButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), Dashboard.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
+        connectionClass = new ConnectionClass();
+        edtuserid = (EditText) findViewById(R.id.userId);
+        edtpass = (EditText) findViewById(R.id.password);
+        btnlogin = (Button) findViewById(R.id.loginButton);
+        pbbar = (ProgressBar) findViewById(R.id.pbbar);
+        pbbar.setVisibility(View.GONE);
 
-
-
-    }
-
-    private void addListenerOnButton() {
-        final Context context = this;
-        lButton = (Button) findViewById(R.id.loginButton);
-        lButton.setOnClickListener(new View.OnClickListener() {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, Dashboard.class);
-                    startActivity(intent);
+                DoLogin doLogin = new DoLogin();
+                doLogin.execute("");
+
             }
         });
     }
 
+
+    public class DoLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+
+
+        String userid = edtuserid.getText().toString();
+        String password = edtpass.getText().toString();
+
+
+        @Override
+        protected void onPreExecute() {
+            pbbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r) {
+            pbbar.setVisibility(View.GONE);
+            Toast.makeText(Login.this,r,Toast.LENGTH_SHORT).show();
+
+            if(isSuccess) {
+                Intent i = new Intent(Login.this, Dashboard.class);
+                startActivity(i);
+                finish();
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(userid.trim().equals("")|| password.trim().equals(""))
+                z = "Please enter User Id and Password";
+            else {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        z = "Error in connection with SQL server";
+                    } else {
+                        String query = "select * from Usertbl where UserId='" + userid + "' and password='" + password + "'";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+
+                        if(rs.next()) {
+                            z = "Login successful";
+                            isSuccess=true;
+                        }
+                        else {
+                            z = "Invalid Credentials";
+                            isSuccess = false;
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    isSuccess = false;
+                    z = "Exceptions";
+                }
+            }
+            return z;
+        }
+    }
 }
 
 
